81767518b5a8f7f53035093ef7c5c0baa2cd2f5c
Brod1ag4 <71493712+Brod1ag4@users.noreply.github.com>
2020-09-19 13:14:42 +0300

    Часы
    ???????????

---
+++ HTML/scripts/Clock.js
@@ -0,0 +1,20 @@
+function Time()
+{
+  var time=new Date();
+  var hour = time.getHours();
+  var min = time.getMinutes();
+  var sec = time.getSeconds();
+  hour = checkTime(hour);
+  min = checkTime(min);
+  sec = checkTime(sec);
+  document.getElementById('clock').innerHTML=hour+":"+min+":"+sec;
+  setTimeout('Time()',500);
+}
+function checkTime(i)
+{
+  if (i<10)
+  {
+    i="0" + i;
+  }
+  return i;
+}
